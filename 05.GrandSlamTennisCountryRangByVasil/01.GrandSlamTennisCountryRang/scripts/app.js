65b8a6ce12631aef0b4d7a2e4c73b6de7b7e172c
Alexander Stoyanov <stoyanovalexander1986@yahoo.com>
2013-10-11 01:32:55 +0200

    The Project

---
+++ 05.GrandSlamTennisCountryRangByVasil/01.GrandSlamTennisCountryRang/scripts/app.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+(function() {
+    
+    document.addEventListener("deviceready", function() {
+        //app.servicesBaseUrl = "http://localhost:62354/api/";
+        app.servicesBaseUrl = "http://localhost:61886/api/";
+        
+        var kendoApp = new kendo.mobile.Application(document.body);
+    });    
+}());
---
+++ 05.GrandSlamTennisCountryRangByVasil/Parser/Models/Continent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parser.Models
+{
+    public class Continent
+    {
+        public string Name { get; set; }
+        public ICollection<Country> Countries { get; set; }
+
+        public Continent()
+        {
+            this.Countries = new HashSet<Country>();
+
+        }
+    }
+}
